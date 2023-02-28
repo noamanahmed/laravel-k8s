ca1d162ad41971712c83748dd0949e758ac0e783
Noaman <noamanahmd@gmail.com>
2023-02-28 15:47:30 +0500

    Added Port Forwarding Script for Mysql

--- 01-mysql/init.sh
+++ 01-mysql/init.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+
+cd /tmp
+version=15.0.2
+file=vitess-${version}-a914f40.tar.gz
+wget https://github.com/vitessio/vitess/releases/download/v${version}/${file}
+tar -xzf ${file}
+cd ${file/.tar.gz/}
+sudo cp -r * /usr/local/bin/
+
+
+cd "$(dirname "$0")"
 /usr/local/bin/kubectl wait --for=condition=Ready apply -f  01-operator.yaml
 /usr/local/bin/kubectl wait --for=condition=Ready apply -f  101_initial_cluster.yaml
